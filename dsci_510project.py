206c2239209bf001ba40ccfd8419d36116a9639c
shivanirusc <166767362+shivanirusc@users.noreply.github.com>
2024-05-02 18:44:51 -0700

    Update dsci_510project.py

--- dsci_510project.py
+++ dsci_510project.py
@@ -81,7 +81,7 @@ colors = px.colors.qualitative.Plotly
 # Plotly chart with custom colors
 fig = px.bar(recipe_counts_df, x="Condition", y="Recipe Count", title="Number of Recipes for Different Conditions",
              color="Condition", color_discrete_sequence=colors)
-fig.update_layout(xaxis_title="Condition", yaxis_title="Recipe Count")
+fig.update_layout(xaxis_title="Health Condition", yaxis_title="Number of Recipes")
 
 # Display the chart
 st.plotly_chart(fig)
