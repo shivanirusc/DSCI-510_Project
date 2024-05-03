48a015a115d3bfe12fb4c4806a43a8ce84256b84
shivanirusc <166767362+shivanirusc@users.noreply.github.com>
2024-05-03 01:44:50 -0700

    Update recipe_filter_app.py

--- recipe_filter_app.py
+++ recipe_filter_app.py
@@ -4,10 +4,12 @@ import pandas as pd
 # Load data
 data = pd.read_csv("updated_recipe_data.csv")
 
+# Set the title of the Streamlit app
 st.title("Recipe Filter")
 
 # Function to classify recipe categories
 def classify_recipe(row):
+    # Check if the recipe name contains keywords to classify it into a category
     if 'soup' in row['Recipe Name'].lower():
         return 'Soup'
     elif 'salad' in row['Recipe Name'].lower():
@@ -21,6 +23,7 @@ def classify_recipe(row):
 
 # Function to filter data based on user inputs
 def filter_data(ingredient, category, health_conditions, sweet_or_drink, allergy_ingredients):
+    # Make a copy of the original data
     filtered_data = data.copy()
     
     # Filter by ingredient
@@ -58,9 +61,10 @@ background_image = 'https://i.imgur.com/AUtA6b0.jpeg'
 # Display background image
 st.image(background_image, use_column_width=True)
 
+# Section for user input
 st.subheader("Enter Your Preferences")
 
-# Input fields
+# Input fields for user preferences
 ingredient = st.text_input("Enter an ingredient", "")
 allergy_ingredients = st.text_input("Enter allergy or restricted ingredients (Enter None if no restricted ingredients)", "")
 allergy_ingredients_list = [ingredient.strip() for ingredient in allergy_ingredients.split(',')]
@@ -68,13 +72,15 @@ category = st.multiselect("Select recipe category", ["Vegetarian", "Non-Vegetari
 health_conditions = st.multiselect("Select health conditions", ["Diabetes", "Low Blood Pressure", "High Blood Pressure", "Low Calorie"])
 sweet_or_drink = st.multiselect("Select Meal Type", ["Sweet Dish", "Drink", "Meal", "Soup"])
 
-# Filter button
+# Button to trigger filtering of recipes
 if st.button("Find Recipes"):
     st.subheader("Filtered Recipes")
+    # Filter the data based on user preferences
     filtered_data = filter_data(ingredient, category, health_conditions, sweet_or_drink, allergy_ingredients_list)
+    # Display the filtered recipes in a table format
     if filtered_data.empty:
         st.write("No recipes found matching the criteria.")
     else:
         # Restrict the resulting recipes to 10
         filtered_data = filtered_data.head(10)
-        st.table(filtered_data)
+        st.table(filtered_data) 
