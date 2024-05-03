32dca4073f6a60072cf2a12423e3fe363755c9de
shivanirusc <166767362+shivanirusc@users.noreply.github.com>
2024-05-02 18:26:38 -0700

    Update dsci_510project.py

--- dsci_510project.py
+++ dsci_510project.py
@@ -57,6 +57,8 @@ graph_one = """
 
 The plot visualizes the number of recipes available for different health conditions, providing insights into the distribution of recipes based on specific health criteria. 
 """
+st.markdown(graph_one, unsafe_allow_html=True)
+
 # Define conditions
 conditions = {
     "High Blood Pressure": data[data["Sodium"] < 150],
@@ -75,13 +77,12 @@ recipe_counts_df = pd.DataFrame(list(recipe_counts.items()), columns=["Condition
 colors = px.colors.qualitative.Plotly
 
 # Plotly chart with custom colors
-fig = px.bar(recipe_counts_df, x="Condition", y="Recipe Count", title="Number of Recipes for Different Conditions",
+fig = px.bar(recipe_counts_df, x="Condition", y="Recipe Count", title=" ",
              color="Condition", color_discrete_sequence=colors)
 fig.update_layout(xaxis_title="Condition", yaxis_title="Recipe Count")
 
 # Display the chart
 st.plotly_chart(fig)
-st.markdown(graph_one, unsafe_allow_html=True)
 
 # Function to classify recipe categories
 def classify_recipe(row):
